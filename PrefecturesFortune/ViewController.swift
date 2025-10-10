cbcda3c3cfe365c2ade82fd6871164abd119ca5f
strovery15 <yutakawamae@gmail.com>
2025-10-10 10:57:52 +0900

    first commit

---
+++ PrefecturesFortune/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrefecturesFortune
+//
+//  Created by 川前優太 on 2025/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
