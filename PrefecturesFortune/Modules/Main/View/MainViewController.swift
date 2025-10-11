82ca93886b60a630b982a139e003a342edded2a7
strovery15 <yutakawamae@gmail.com>
2025-10-12 03:05:30 +0900

    feature-入力されたデータを表示するView(CurrentDataView)を作った
    #MainViewのuserDataのdidSetでCurrentDataViewを更新する

--- PrefecturesFortune/Modules/Main/View/MainViewController.swift
+++ PrefecturesFortune/Modules/Main/View/MainViewController.swift
@@ -8,6 +8,14 @@ protocol MainView: AnyObject {
 
 class MainViewController: UIViewController {
     
+    var presenter: MainPresentation!
+    
+    var userDate: UserData? {
+        didSet {
+            currentDataView.userData = userDate
+        }
+    }
+    
     @IBOutlet weak var startButton: UIButton! {
         didSet {
             configureStartButton()
@@ -20,18 +28,23 @@ class MainViewController: UIViewController {
         }
     }
     
-    var presenter: MainPresentation!
+    var currentDataView: CurrentDataView! {
+        didSet {
+            configureCurrentDataView()
+        }
+    }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         firstConfiguration()
-        
     }
     
     func firstConfiguration() {
+        currentDataView = CurrentDataView()
+        view.addSubview(currentDataView)
         
+        NotificationCenter.default.addObserver(self, selector: #selector(notifyUserData(_:)), name: .notifyUserData, object: nil)
     }
     
     @IBAction func startButtonAction(_ sender: Any) {
@@ -40,6 +53,11 @@ class MainViewController: UIViewController {
     
     @IBAction func setDataButtonAction(_ sender: Any) {
         presenter.setData()
+    }
+    
+    @objc func notifyUserData(_ notification: Notification) {
+        let data = notification.userInfo!["userData"] as! UserData
+        self.userDate = data
     }
     
 }
@@ -65,5 +83,14 @@ extension MainViewController {
         setDataButton.backgroundColor = .systemBlue
         setDataButton.layer.cornerRadius = 8
     }
+    
+    //currentDataView
+    func configureCurrentDataView() {
+        currentDataView.layer.borderColor = UIColor.systemPurple.cgColor
+        currentDataView.layer.borderWidth = 1
+        currentDataView.layer.cornerRadius = 10
+        currentDataView.frame = CGRect(x: 10, y: 100, width: 250, height: 160)
+    }
+    
 }
 
